24a7a94276090bea12055f5c7385dcf71af1d074
congzengguang <congzengguang@sina.cn>
2018-05-22 22:10:02 +0800

    第一个java程序

---
+++ java-basic/src/main/java/com/test/basic/HelloWorld.java
@@ -0,0 +1,13 @@
+package com.test.basic;
+
+/**
+ * 第一个java程序
+ * @author czg
+ * @date 2018/5/22
+ */
+public class HelloWorld {
+    public static void main(String args[]){
+        System.out.println("Hello World!");
+    }
+}
+
