b0ee9454e791e945e7e8987962a5dd7d27222f37
congzengguang <congzengguang@sina.cn>
2018-05-22 22:15:50 +0800

    静态方法调用

---
+++ java-basic/src/main/java/com/test/basic/StaticMethod.java
@@ -0,0 +1,15 @@
+package com.test.basic;
+
+/**
+ * 静态方法调用
+ * @author czg
+ * @date 2018/5/22
+ */
+public class StaticMethod {
+    public static void main(String args[]){
+        print();
+    }
+    public static void print(){
+        System.out.println("Hello World !");
+    }
+}
