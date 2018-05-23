e6747d88f2208767fc315dc4e0ca3a7cdf7c61c1
congzengguang <congzengguang@sina.cn>
2018-05-23 20:31:45 +0800

    Integer 类型转换

---
+++ java-basic/src/main/java/com/test/basic/IntegerTest.java
@@ -0,0 +1,18 @@
+package com.test.basic;
+
+/**
+ * @author czg
+ * @date 2018/5/23
+ */
+public class IntegerTest {
+    public static void main(String args[]){
+        int a = 10;
+        int b = 20;
+        int c = a/b;
+        double d = a/b;
+        double e = (double)a/b;
+        System.out.println(c);
+        System.out.println(d);
+        System.out.println(e);
+    }
+}
