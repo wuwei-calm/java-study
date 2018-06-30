28f847d487d9e956fc1fc446516aac6afd5ecae6
congzengguang <congzengguang@sina.cn>
2018-06-30 17:25:34 +0800

    -128 到127的  整型自动转换

---
+++ java-basic/src/main/java/com/test/basic/IntegerCompareInt.java
@@ -0,0 +1,14 @@
+package com.test.basic;
+
+/**
+ * @author czg
+ * @date 2018/6/30
+ */
+public class IntegerCompareInt {
+    public static void main(String args[]){
+        Integer  a= -1;
+        if(a == -1){
+            System.out.println(true);
+        }
+    }
+}
